d383c8ff72e0011e27f050ea0661e2154c949ccc
Emmanuelprime <emmanuelprime2@gmail.com>
2024-08-19 18:28:51 +0100

    starte urdf

---
+++ build/chessbot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/emmanuel/Chess-Bot-ROS2/src/chessbot_description
+# Build directory: /home/emmanuel/Chess-Bot-ROS2/build/chessbot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
